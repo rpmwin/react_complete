778d57648f2199962c2960a2d1b77df517ca59a4
RISHIK PUNEET M <Rishikpuneetm@gmail.com>
2024-02-01 20:36:42 +0530

    a new commit from the new resetted pc

--- FarmersConnect/src/App.jsx
+++ FarmersConnect/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='bg-slate-600 min-h-screen w-full h-full text-white flex justify-center items-center'>
-  <Outlet className="bg-slate-600 min-h-screen w-full h-full  " />
-</div>
+    <div className="bg-slate-600 min-h-screen w-full h-full text-white flex justify-center items-center">
+      <Outlet className="bg-slate-600 min-h-screen w-full h-full  " />
+    </div>
   );
 }
 
